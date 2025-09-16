a80071ad24c23c7f282f3975866e9e21601b483d
Yesenia <lizet.peche@gmail.com>
2025-09-15 23:26:45 -0500

    se modifica properties, pom y test para jenkins

--- account-ms/src/test/java/com/bank/accountms/AccountMsApplicationTests.java
+++ account-ms/src/test/java/com/bank/accountms/AccountMsApplicationTests.java
@@ -3,7 +3,7 @@ package com.bank.accountms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AccountMsApplicationTests {
 
 	@Test
--- customer-ms/src/test/java/com/bank/customerms/CustomerMsApplicationTests.java
+++ customer-ms/src/test/java/com/bank/customerms/CustomerMsApplicationTests.java
@@ -3,7 +3,7 @@ package com.bank.customerms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerMsApplicationTests {
 
 	@Test
